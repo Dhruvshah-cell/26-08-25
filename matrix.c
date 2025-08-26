9855dce6fab8f1d2841ff82e95e4f13064de7154
Dhruvshah-cell <nitinshah073@gmail.com>
2025-08-26 17:43:49 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int rows,cols;
+printf("enter no of rows");
+scanf("%d",&rows);
+printf("enter number of columns");
+scanf("%d",&cols);
+for(int i=1;i<=rows;i++)
+{
+for(int j=1;j<=cols;j++)
+{
+printf("(%d,%d)",i,j);
+}
+printf("\n");
+}
+return 0;
+}
